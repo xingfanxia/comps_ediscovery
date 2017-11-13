9819ff567849b58bc464d909f28a000968fd58ac
nachtm <nachtm@carleton.edu>
2017-11-12 20:18:17 -0600

    filtering now works as well

--- web/static/js/documentview.js
+++ web/static/js/documentview.js
@@ -49,6 +49,60 @@ var store = {
   }
 }
 
+// register the grid component
+Vue.component('demo-grid', {
+  template: '#grid-template',
+  delimiters: ['[[',']]'],
+  props: {
+    data: Array,
+    columns: Array,
+    filterKey: String
+  },
+  data: function () {
+    var sortOrders = {}
+    this.columns.forEach(function (key) {
+      sortOrders[key] = 1
+    })
+    return {
+      sortKey: '',
+      sortOrders: sortOrders
+    }
+  },
+  computed: {
+    filteredData: function () {
+      var sortKey = this.sortKey
+      var filterKey = this.filterKey && this.filterKey.toLowerCase()
+      var order = this.sortOrders[sortKey] || 1
+      var data = this.data
+      if (filterKey) {
+        data = data.filter(function (row) {
+          return Object.keys(row).some(function (key) {
+            return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+          })
+        })
+      }
+      if (sortKey) {
+        data = data.slice().sort(function (a, b) {
+          a = a[sortKey]
+          b = b[sortKey]
+          return (a === b ? 0 : a > b ? 1 : -1) * order
+        })
+      }
+      return data
+    }
+  },
+  filters: {
+    capitalize: function (str) {
+      return str.charAt(0).toUpperCase() + str.slice(1)
+    }
+  },
+  methods: {
+    sortBy: function (key) {
+      this.sortKey = key
+      this.sortOrders[key] = this.sortOrders[key] * -1
+    }
+  }
+})
 
 window.onload = function () {
   documentView = new Vue({
@@ -65,77 +119,89 @@ window.onload = function () {
     }
   })
 
-  emailList = new Vue({
+  var emailList = new Vue({
     el: '#emailList',
-    delimiters: ["[[", "]]"],
-    data: function(){
-      console.log('data')
-      var sortOrders = {}
-      var keys = ['Sender', 'Receiver', 'Subject', 'Sent_Date']
-      keys.forEach(function (key) {
-        sortOrders[key] = 1
-      })
-      console.log('data end')
-      var toReturn = {
-        items: [
+    delimiters: ['[[',']]'],
+    data: {
+      searchQuery: '',
+      gridColumns: ['Sender', 'Receiver', 'Subject', 'Sent_Date'],
+      gridData: [
           { Sender: 'John Doe', Receiver : 'Jill Doe', Subject : 'Pizza Tonight?', Sent_Date : '1/1/17', Contents : "Yo", id : 0 },
           { Sender: 'Bob Doe', Receiver : 'John Doe', Subject : 'Pasta Tonight?', Sent_Date : '2/1/17', Contents : "hell0", id : 1 },
           { Sender: 'Elliot Doe', Receiver : 'Randy Doe', Subject : '\'za Tonight?', Sent_Date : '3/1/17', Contents : "whats up", id : 2 },
           { Sender: 'John Doe', Receiver : 'Jill Doe', Subject : 'Yolo', Sent_Date : '4/1/17', Contents : "butts", id : 3 }
-        ],
-        keys: keys,
-        sortKey: '',
-        sortOrders: sortOrders
-      }
-      console.log(toReturn)
-      return toReturn 
-    },
-    methods: {
-        alert_user: function(item) {
-          // `this` inside methods points to the Vue instance
-          store.setMessageAction(item)
-          documentView.updateView()
-          // `event` is the native DOM event
-          if (event) {
-            //alert(event.target.tagName)
-          }
-        },
-        sortBy: function (key) {
-          console.log('sorting by', key);
-          this.sortKey = key
-          this.sortOrders[key] = this.sortOrders[key] * -1
-        }
-    },
-    computed: {
-      filteredData: function () {
-        var sortKey = this.sortKey
-        var filterKey = this.filterKey && this.filterKey.toLowerCase()
-        var order = this.sortOrders[sortKey] || 1
-        var data = this._data.items
-        console.log('data:',data)
-        if (filterKey) {
-          console.log('filterKey')
-          data = data.filter(function (row) {
-            return Object.keys(row).some(function (key) {
-              return String(row[key]).toLowerCase().indexOf(filterKey) > -1
-            })
-          })
-        }
-        if (sortKey) {
-          console.log('sortKey')
-          data = data.slice().sort(function (a, b) {
-            a = a[sortKey]
-            b = b[sortKey]
-            return (a === b ? 0 : a > b ? 1 : -1) * order
-          })
-        }
-        return data
-      }
-    },
-    filters: {
-      capitalize: function (str) {
-        return str.charAt(0).toUpperCase() + str.slice(1)
-      }
+        ]
     }
   })
+
+  // emailList = new Vue({
+  //   el: '#emailList',
+  //   delimiters: ["[[", "]]"],
+  //   data: function(){
+  //     var sortOrders = {}
+  //     var keys = ['Sender', 'Receiver', 'Subject', 'Sent_Date']
+  //     keys.forEach(function (key) {
+  //       sortOrders[key] = 1
+  //     })
+  //     var toReturn = {
+  //       items: [
+  //         { Sender: 'John Doe', Receiver : 'Jill Doe', Subject : 'Pizza Tonight?', Sent_Date : '1/1/17', Contents : "Yo", id : 0 },
+  //         { Sender: 'Bob Doe', Receiver : 'John Doe', Subject : 'Pasta Tonight?', Sent_Date : '2/1/17', Contents : "hell0", id : 1 },
+  //         { Sender: 'Elliot Doe', Receiver : 'Randy Doe', Subject : '\'za Tonight?', Sent_Date : '3/1/17', Contents : "whats up", id : 2 },
+  //         { Sender: 'John Doe', Receiver : 'Jill Doe', Subject : 'Yolo', Sent_Date : '4/1/17', Contents : "butts", id : 3 }
+  //       ],
+  //       keys: keys,
+  //       sortKey: '',
+  //       sortOrders: sortOrders,
+  //       searchQuery: ''
+  //     }
+  //     return toReturn 
+  //   },
+  //   methods: {
+  //       alert_user: function(item) {
+  //         // `this` inside methods points to the Vue instance
+  //         store.setMessageAction(item)
+  //         documentView.updateView()
+  //         // `event` is the native DOM event
+  //         if (event) {
+  //           //alert(event.target.tagName)
+  //         }
+  //       },
+  //       sortBy: function (key) {
+  //         console.log('sorting by', key);
+  //         this.sortKey = key
+  //         this.sortOrders[key] = this.sortOrders[key] * -1
+  //       }
+  //   },
+  //   computed: {
+  //     filteredData: function () {
+  //       var sortKey = this.sortKey
+  //       var filterKey = this.filterKey && this.filterKey.toLowerCase()
+  //       var order = this.sortOrders[sortKey] || 1
+  //       var data = this.items
+  //       if (filterKey) {
+  //         console.log('filterKey')
+  //         data = data.filter(function (row) {
+  //           return Object.keys(row).some(function (key) {
+  //             return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+  //           })
+  //         })
+  //       }
+  //       if (sortKey) {
+  //         console.log('sortKey')
+  //         data = data.slice().sort(function (a, b) {
+  //           a = a[sortKey]
+  //           b = b[sortKey]
+  //           return (a === b ? 0 : a > b ? 1 : -1) * order
+  //         })
+  //       }
+  //       return data
+  //     }
+  //   },
+  //   filters: {
+  //     capitalize: function (str) {
+  //       return str.charAt(0).toUpperCase() + str.slice(1)
+  //     }
+  //   }
+  // })
 }
