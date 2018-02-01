73a5e86f5d6b386acfe0d772742e2eba390e2a3a
nachtm <nachtm@carleton.edu>
2018-02-01 11:56:03 -0600

    Did some visualization of the parameter sweep results

--- kimj4_exps/param_process.py
+++ kimj4_exps/param_process.py
@@ -1,6 +1,7 @@
 import os
+import pprint
 
-directory_in_str = '/home/juyun/param_test/param/'
+directory_in_str = '../data/params/param/'
 directory = os.fsencode(directory_in_str)
 
 param_metrics_dict = {}
@@ -17,21 +18,21 @@ for file in os.listdir(directory):
         fit_time = 0
         predict_time = 0
         with open(directory_in_str + filename, 'r') as f:
-            print(filename.split('.txt')[0])
+            # print(filename.split('.txt')[0])
             for line in f:
                 if 'average' in line:
                     if 'precision' in line:
-                        precision = line.split()[-1]
+                        precision = float(line.split()[-1])
                     elif 'recall' in line:
-                        recall = line.split()[-1]
+                        recall = float(line.split()[-1])
                     elif 'accuracy' in line:
-                        accuracy = line.split()[-1]
+                        accuracy = float(line.split()[-1])
                     elif 'f1' in line:
-                        f1 = line.split()[-1]
+                        f1 = float(line.split()[-1])
                     elif 'fit time' in line:
-                        fit_time = line.split()[-1]
+                        fit_time = float(line.split()[-1])
                     elif 'predict_time' in line:
-                        predict_time = line.split()[-1]
+                        predict_time = float(line.split()[-1])
                     else:
                         a = 1
 
@@ -41,5 +42,35 @@ for file in os.listdir(directory):
         continue
     else:
         continue
-for val in param_metrics_dict.values():
-    print(val)
+
+#get the sum of precision, acc, etc and the total number of occurrences to calculate averages by # of trees
+# tree_totals = {}
+# tree_nums = {}
+# print('yo')
+pp = pprint.PrettyPrinter()
+# pp.pprint(param_metrics_dict)
+# for key in param_metrics_dict.keys():
+#     num_trees = key.split('.')[0]
+#     try:
+#         tree_totals[num_trees] = tuple(map(sum, zip(tree_totals[num_trees], param_metrics_dict[key])))
+#         tree_nums[num_trees] += 1
+#     except KeyError:
+#         tree_totals[num_trees]  = param_metrics_dict[key]
+#         tree_nums[num_trees] = 1
+# pp.pprint(tree_totals)
+# pp.pprint(tree_nums)
+# for key in sorted(tree_totals.keys(), key=lambda x: int(x)):
+#     av = tuple(map(lambda x: float(x)/tree_nums[key], tree_totals[key]))
+#     print('{} Trees:-------------------------'.format(key))
+#     print('Precision {}\n recall {}\n accuracy {}\n f1 {}\n fit time {}\n predicttime {}'.format(*av))
+
+#find best f1 score
+score_tuples = []
+for key in param_metrics_dict.keys():
+    score_tuples.append((key, *param_metrics_dict[key]))    
+print('Trees,tree_depth,features,precision,recall,accuracy,f1,fit_time')
+for setting in score_tuples:
+    # print('{}'.format(setting[0]))    
+    # print('precision {}\n recall {}\n accuracy {}\n f1 {}\n fit time {}\n predicttime {}'.format(*setting[1:]))
+    settings = (setting[0].split('.'))
+    print('{},{},{},{},{},{},{},{}'.format(*(*settings, *setting[1:6])))
