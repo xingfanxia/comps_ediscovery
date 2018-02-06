7559e4744643c8eeb0d5cfd03172cf10733d5158
xxf1995 <xiax@carleton.edu>
2018-02-06 15:50:44 -0600

    [feat] better visual trace with opacity instead of feedback buttons; also only changes button style after feedback is logged

--- web_new/src/components/result/CustomActions.vue
+++ web_new/src/components/result/CustomActions.vue
@@ -1,8 +1,8 @@
 // CustomActions.vue
 <template>
   <div class="custom-actions">
-    <button class="ui basic button" @click="feedback(rowData, 1); isCActive = !isCActive"><i class="check icon" v-bind:class="{ CActive: isCActive }"></i></button>
-    <button class="ui basic button" @click="feedback(rowData, 0); isXActive = !isXActive"><i class="x icon" v-bind:class="{ XActive: isXActive }"></i></button>
+    <button class="ui basic button" @click="feedback(rowData, 1, 0);"><i class="check icon" v-bind:class="{ CActive: isCActive }"></i></button>
+    <button class="ui basic button" @click="feedback(rowData, 0, 1);"><i class="x icon" v-bind:class="{ XActive: isXActive }"></i></button>
   </div>
 </template>
 
@@ -29,13 +29,20 @@ export default {
   },
 
   methods: {
-    feedback: function (rowData, relevant) {
+    feedback: function (rowData, relevant, flag) {
+      var vm = this;
+
       axios.post('http://127.0.0.1:5000/feedback', {
         'ID': rowData.ID,
         'Relevant': relevant
       })
         .then(function (response) {
           console.log(response.data['message'])
+          if (flag === 0) {
+            vm.isCActive = !vm.isCActive
+          } else {
+            vm.isXActive = !vm.isXActive
+          }
         })
         .catch(function (error) {
           console.log(error)
@@ -53,14 +60,20 @@ export default {
   .custom-actions button.ui.button {
     padding: 8px 8px;
   }
+
   .custom-actions button.ui.button > i.icon {
     margin: auto !important;
   }
+
+  i {
+    color: rgba(0, 0, 0, 0.1);
+  }
+
   .CActive {
-    color: #99c778;
+    color: rgba(0, 0, 0, 1);
   }
 
   .XActive {
-    color: #c2333c;
+    color: rgba(0, 0, 0, 1);
   }
 </style>
