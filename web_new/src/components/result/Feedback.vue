1646d31401a04cd8559c98af55482042e71171a7
tigar <aatigar@gmail.com>
2018-02-23 17:27:19 -0600

    [feat] trying greyout

--- web_new/src/components/result/Feedback.vue
+++ web_new/src/components/result/Feedback.vue
@@ -1,21 +1,38 @@
 <template>
   <section id="feedback">
+    <!-- <div id="greyscreen">
+    </div> -->
+    <loading :active.sync="visible" :can-cancel="true"></loading>
     <div id="buttons">
       <button class="ui basic button" @click="learn()">Run Incremental Learning</button>
     </div>
+
   </section>
 </template>
 
 <script>
 
 import axios from 'axios'
+import loading from 'vue-loading-overlay'
 
 export default {
+  data() {
+     return {
+      visible: false
+    }
+  },
+
+  components: {
+    'loading': loading
+  },
   methods: {
     learn: function () {
+      var vm = this
       alert('Incremental learning starts, please wait patiently!')
+      vm.visible = true
       axios.get('http://127.0.0.1:5000/dbtest')
         .then(function (response) {
+          vm.visble = false
           if (response.data['status_code'] === 500) {
             alert(response.data['message'])
           } else {
@@ -32,3 +49,15 @@ export default {
 }
 
 </script>
+
+<style scoped>
+#greyscreen {
+  background-color: gray;
+  position:absolute;
+  width: 1000px;
+  height: 1000px;
+  z-index: 99;
+  top: -800px;
+  left: 0px;
+}
+</style>
